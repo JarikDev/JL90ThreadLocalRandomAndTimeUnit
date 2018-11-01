b8179f21c39127c71f6a9550176a20d0355ece35
ivan4000 <sxln1@mail.ru>
2018-11-01 18:35:18 +0300

    JL90ThreadLocalRandomAndTimeUnit

---
+++ src/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(TimeUnit.DAYS.toMillis(14));
+    }
+}
